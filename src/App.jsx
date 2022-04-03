dac2a03fa513a412019aad782be1f252a772412d
Taha <tahajamshaid1234@gmail.com>
2022-04-03 23:40:07 +0500

    Header done

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import { Brand, CTA, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+// css Naming is BEM
+// BEM => Block Element Modifier
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
