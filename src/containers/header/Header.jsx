6078ff2b27b1b1be383d61e417ba6653af2b0857
Taha <tahajamshaid1234@gmail.com>
2022-05-07 00:45:42 +0500

    what GPT3 is DONE

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
-      <div className="gpt3_header-image">
+      <div className="gpt3__header-image">
         <img src={ai} alt="Ai Mask" />
       </div>
     </div>
